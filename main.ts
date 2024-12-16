61254aa31862ec02067ed67d3ae25985bb88a89d
lukass2010 <lratamanik@my.ecsrd.ca>
2024-12-16 09:46:36 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    music.play(music.stringPlayable("C5 A C5 B C5 C5 C5 C5 ", 98), music.PlaybackMode.LoopingInBackground)
+})
